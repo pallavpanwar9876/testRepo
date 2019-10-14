8dd9472bca66277daf839f964bf98693dffb00eb
shobro <shobhitbelwal02@gmail.com>
2019-10-15 03:02:45 +0530

    add to your repository

---
+++ program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a1,a2,s;	a1=0;
+	a2=1;
+	printf("Enter the limit upto which you want to see fibonacci series");
+	scanf("%d"&s)
+	int sum;
+	for(int i=2;i<=n;++i)
+	{	printf("%d",a1+a2);
+		a1=a2;
+		a1=a1+a2;
+	}
+return 0;
+}
